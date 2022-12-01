5ff48a4198b5eb160b03153f4bd9e41e13fffbb2
oriolsastre <oriol.sastre@gmail.com>
2022-12-01 12:14:01 +0100

    barrescontrabarres

--- 20221129_barrescontrabarres.js
+++ 20221129_barrescontrabarres.js
@@ -1,17 +1,19 @@
 function verticalShalsh(barres){
-    var rawbarres = String.raw`${barres}`
-    console.log(rawbarres)
     var decalatge = 0;
     var minDecalatge = 0;
-    for(let i=0;i<rawbarres.length;i++){
-        if(rawbarres.charAt(i)=='/'){decalatge++;if(decalatge>minDecalatge){minDecalatge=decalatge;}}
-        else if(rawbarres.charAt(i)=='\\'){decalatge--;}
+    for(let i=0;i<barres.length;i++){
+        if(barres.charAt(i)=='/'){decalatge++;if(decalatge>minDecalatge){minDecalatge=decalatge;}}
+        else if(barres.charAt(i)=='\\'){decalatge--;}
         else{return console.log("L'input només poden ser barres '/' i contrabarres '\\'");}
     }
-    for(let i=0;i<rawbarres.length;i++){
-        if(rawbarres.charAt(i)=='\\'){console.log(" ".repeat(minDecalatge)+" "+rawbarres.charAt(i));minDecalatge++;}
-        else if(rawbarres.charAt(i)=='/'){console.log(" ".repeat(minDecalatge)+rawbarres.charAt(i));minDecalatge--;}
+    for(let i=0;i<barres.length;i++){
+        if(barres.charAt(i)=='\\'){console.log(" ".repeat(minDecalatge)+" "+barres.charAt(i));minDecalatge++;}
+        else if(barres.charAt(i)=='/'){console.log(" ".repeat(minDecalatge)+barres.charAt(i));minDecalatge--;}
     }
 }
 
-verticalShalsh('//\///a\\')
+function stringEscape(s) {
+    return s ? s.replace(/\\/g,'\\\\') : s;
+}
+
+verticalShalsh(String.raw`//\///\\`)
