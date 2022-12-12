38f01877664dfececfb9d4cec2a4890a8f667684
Oriol Sastre <oriol.sastre@gmail.com>
2022-12-12 21:01:53 +0100

    catan

--- 20221212_catan.js
+++ 20221212_catan.js
@@ -1,4 +1,4 @@
-function randomCatan(){
+/*(function randomCatan(){
     var hexagons = [2,3,3,4,4,5,5,6,6,8,8,9,9,'A','A','B','B','C','.'];
     const filesTauler = [3,4,5,4,3];
     var taulerArray = [[],[],[],[],[]]; 
@@ -12,7 +12,7 @@ function randomCatan(){
                 var proves = 0;
                 var validTile = false;
                 while(!validTile){
-                    if(proves > hexagons.length){validTile=true;continuar=false;}
+                    if(proves > 5*hexagons.length){validTile=true;continuar=false;}
                     if(hexagons[hexagonRnd]===8 || hexagons[hexagonRnd]===6){
                         if(hexagons[hexagonRnd]===8){var oposite = 6;}else{var oposite = 8;}
                         if(i>0 && taulerArray[index][i-1]!=oposite){
@@ -50,6 +50,20 @@ function randomCatan(){
     }else{
         randomCatan()
     }
-}
+})()*/
 
-randomCatan();
+var n = [6,9,-7,3,8,-1,-6,-7,5,-7,7,-7,-1,4,7,9];
+var m = [-1,8,-7,2];
+
+(function sumaRectangle(n,m){
+    if(!Array.isArray(n) || !Array.isArray(m)){return console.log("Els paràmetres no són arrays.");}
+    if(m.length!=4){return console.log("El segon array ha de ser de mida 4 representant les 4 cantonades d'un rectangle.")}
+    
+    var a = n.length**(1/2);
+    if(!Number.isInteger(a)){return console.log("De moment el primer array ha de representar un quadrat. No és el cas")}
+    var b = a;
+    
+
+
+
+})(n,m)
