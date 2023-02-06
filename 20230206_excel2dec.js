cf0026f9631176e7e1fb4cec2f6ec8a830402c37
Oriol Sastre <oriol.sastre@gmail.com>
2023-02-06 17:02:27 +0100

    minor change

--- 20230206_excel2dec.js
+++ 20230206_excel2dec.js
@@ -2,10 +2,9 @@ const excel2dec = excel => {
     const alphabet = [0,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     let power=0;
     let dec = 0;
-    for(let i=(excel.length-1);i>=0;i--){
-        let num = alphabet.indexOf(excel[i]);
+    for(let i=excel.length;i>0;i-- && power++){
+        let num = alphabet.indexOf(excel[i-1]);
         dec += num*Math.pow(26,power)
-        power++;
     }
     return dec;
 }
